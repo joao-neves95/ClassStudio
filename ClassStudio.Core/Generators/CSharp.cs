367a7ae60c843398844d8d8e53be8b503cfd4a32
João Neves <joao95neves@gmail.com>
2019-08-10 22:30:12 +0100

    Added the CSharp to XML generator

---
+++ ClassStudio.Core/Generators/CSharp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace ClassStudio.Core.Generators
+{
+    public static class CSharp
+    {
+        public static string ToXML<T>(T classInput)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer( typeof( T ) );
+            StringWriter stringWriter = new StringWriter();
+            xmlSerializer.Serialize( stringWriter, classInput );
+
+            return stringWriter.ToString();
+        }
+
+        public static string ToJSON()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
