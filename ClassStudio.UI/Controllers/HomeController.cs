41c3379ceee5a6accadd6e98816f09beb5a3715d
João Neves <joao95neves@gmail.com>
2019-08-10 21:26:07 +0100

    Added about page

--- ClassStudio.UI/Controllers/HomeController.cs
+++ ClassStudio.UI/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace ClassStudio.UI.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
