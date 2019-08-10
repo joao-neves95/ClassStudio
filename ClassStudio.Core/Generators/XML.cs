52c41ea3204c1b1a725dfc9aa2d8688af44e01c0
João Neves <joao95neves@gmail.com>
2019-08-10 22:27:39 +0100

    Added the XML to CSharp generator

---
+++ ClassStudio.Core/Generators/XML.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Linq;
+using Xml2CSharp;
+using System.Threading.Tasks;
+
+namespace ClassStudio.Core.Generators
+{
+    public static class XML
+    {
+        public static string ToCSharp(string xml)
+        {
+            ClassInfoWriter classInfoWriter = new ClassInfoWriter(
+                new Xml2CSharpConverer().Convert( xml )
+            );
+
+            StringWriter stringWriter = new StringWriter();
+            classInfoWriter.Write( stringWriter );
+
+            return stringWriter.ToString();
+        }
+    }
+}
