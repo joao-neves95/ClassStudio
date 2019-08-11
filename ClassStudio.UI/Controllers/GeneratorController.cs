1a552bc93910357a858845cbda4a7908061502ad
João Neves <joao95neves@gmail.com>
2019-08-11 22:33:54 +0100

    Added the Generator API controller for code generation

---
+++ ClassStudio.UI/Controllers/GeneratorController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClassStudio.UI.Controllers
+{
+    [Route("api/generator")]
+    [ApiController]
+    public class GeneratorController : ControllerBase
+    {
+        [HttpPost]
+        public async Task XMLStringToCSharp(string xml)
+        {
+
+        }
+    }
+}
---
+++ ClassStudio.UI/Enums/LangEnym.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClassStudio.UI.Enums
+{
+    public enum LangEnym
+    {
+        XML = 1,
+        CSharp = 2,
+        TypeScript = 3,
+        JavaScript = 4,
+        JSON = 5
+    }
+}
