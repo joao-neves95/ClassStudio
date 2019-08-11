2ce909c28079b2f1efd0ed1a1a0b64422851d1be
João Neves <joao95neves@gmail.com>
2019-08-11 22:31:34 +0100

    Added a constants object module

---
+++ ClassStudio.UI/Scripts/constants.ts
@@ -0,0 +1,10 @@
+﻿
+export const Constants = Object.freeze({
+  Ids: {
+    Generator: {
+      inputSelector: 'input-selector',
+      outputSelector: 'input-selector',
+      compileBtn: 'compile-btn'
+    }
+  }
+});
