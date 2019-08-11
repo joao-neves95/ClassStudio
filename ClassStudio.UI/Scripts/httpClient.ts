e2608dadf57707e82069fadcb8e75ca4d84edea1
João Neves <joao95neves@gmail.com>
2019-08-11 22:33:17 +0100

    Added an http client module
    [IN PROGRESS]

---
+++ ClassStudio.UI/Scripts/httpClient.ts
@@ -0,0 +1,15 @@
+﻿
+export class HttpClient {
+  public static async get( url ): Promise<Response> {
+    return await fetch( url, {
+      method: 'GET',
+    } );
+  }
+
+  public static async post( url, data ): Promise<Response> {
+    return await fetch( url, {
+      method: 'POST',
+      body: data
+    } );
+  }
+}
