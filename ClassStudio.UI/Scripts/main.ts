cec88f616bdca89148096c71fd5b8de21a5f5a64
João Neves <joao95neves@gmail.com>
2019-08-15 23:08:26 +0100

    Fixed the main.ts module

--- ClassStudio.UI/Scripts/main.ts
+++ ClassStudio.UI/Scripts/main.ts
@@ -1,9 +1,18 @@
-﻿
+﻿/*
+ * Copyright (c) 2019 João Pedro Martins Neves (shivayl) - All Rights Reserved.
+ *
+ * ClassStudio is licensed under the GNU Lesser General Public License (LGPL),
+ * version 3, located in the root of this project, under the name "LICENSE.md".
+ *
+ */
+
+
 import { GeneratorController } from './Generator/generator.controller';
 
-function main() {
-  console.log( 'main.ts' );
+export function main() {
+
   new GeneratorController();
+
 }
 
 main();
