8b2561328126c0800b3ee0dbc3a172da54b80337
João Neves <joao95neves@gmail.com>
2019-08-15 23:05:02 +0100

    Fixes on the .ts httpClient
    - Added headers.
    - Other fixes.

--- ClassStudio.UI/Scripts/httpClient.ts
+++ ClassStudio.UI/Scripts/httpClient.ts
@@ -1,15 +1,30 @@
-﻿
+﻿/*
+ * Copyright (c) 2019 João Pedro Martins Neves (shivayl) - All Rights Reserved.
+ *
+ * ClassStudio is licensed under the GNU Lesser General Public License (LGPL),
+ * version 3, located in the root of this project, under the name "LICENSE.md".
+ *
+ */
+
+
 export class HttpClient {
-  public static async get( url ): Promise<Response> {
+  public static async get( url: string ): Promise<Response> {
     return await fetch( url, {
-      method: 'GET',
+      headers: {
+        'Accept': 'application/json, text/plain',
+      },
+      method: 'GET'
     } );
   }
 
-  public static async post( url, data ): Promise<Response> {
+  public static async post( url: string, data: any ): Promise<Response> {
     return await fetch( url, {
+      headers: {
+        'Accept': 'application/json, text/plain',
+        'Content-Type': 'application/json;charset=UTF-8'
+      },
       method: 'POST',
-      body: data
+      body: JSON.stringify( data )
     } );
   }
 }
