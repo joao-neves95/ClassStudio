0ca519987029c1051e9a57aae97e3241e222adfc
João Neves <joao95neves@gmail.com>
2019-08-15 23:00:54 +0100

    Created the XMLStringToCSharpDTO for the XMLStringToCSharp endpoint

---
+++ ClassStudio.UI/Models/DTO/XMLStringToCSharpDTO.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Copyright (c) 2019 João Pedro Martins Neves (shivayl) - All Rights Reserved.
+ *
+ * ClassStudio is licensed under the GNU Lesser General Public License (LGPL),
+ * version 3, located in the root of this project, under the name "LICENSE.md".
+ *
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClassStudio.UI.Models.DTO
+{
+    public class XMLStringToCSharpDTO
+    {
+        [Required]
+        public string XML { get; set; }
+    }
+}
