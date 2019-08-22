f0ad75ce41af80d30df76022c34b9b1506bf444b
joao-neves95 <joao95neves@gmail.com>
2019-08-22 18:08:48 +0100

    [UI.Startup] - Remove .AddJsonOptions()

--- ClassStudio.UI/Startup.cs
+++ ClassStudio.UI/Startup.cs
@@ -31,11 +31,11 @@ namespace ClassStudio.UI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews()
-                    .AddJsonOptions( options =>
-                    {
-                        options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
-                    } );
+            services.AddControllersWithViews();
+                    //.AddJsonOptions( options =>
+                    //{
+                    //    options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+                    //} );
 
             services.AddRazorPages();
         }
