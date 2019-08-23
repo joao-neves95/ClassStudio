fce9a86cb3797e1ece9dd0193bc1acddd287e80c
João Neves <joao95neves@gmail.com>
2019-08-23 02:29:37 +0100

    [UI.GeneratorController] - Implemented the CSharpToTypescript endpoint

---
+++ ClassStudio.UI/Models/DTO/CSharpToTypescript.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClassStudio.UI.Models.DTO
+{
+    public class CSharpToTypescript
+    {
+        public string Typescript { get; set; }
+    }
+}
