0368940a27e26e06f0b215e71d11f999f8818523
João Neves <joao95neves@gmail.com>
2019-08-29 21:21:22 +0100

    [TSGeneratorSettings]: Added new lines between the properties.

--- ClassStudio.Core/Configuration/TSGeneratorSettings.cs
+++ ClassStudio.Core/Configuration/TSGeneratorSettings.cs
@@ -9,10 +9,15 @@ namespace ClassStudio.Core.Configuration
     public class TSGeneratorSettings : ISettingStore
     {
         public bool AddIPrefixInterfaceDeclaration { get { return true; } set => throw new NotImplementedException(); }
+
         public bool IsConvertListToArray { get { return true; } set => throw new NotImplementedException(); }
+
         public bool IsConvertMemberToCamelCase { get { return false; } set => throw new NotImplementedException(); }
+
         public bool IsConvertToInterface { get { return true; } set => throw new NotImplementedException(); }
+
         public bool IsInterfaceOptionalProperties { get { return false; } set => throw new NotImplementedException(); }
+
         public TypeNameReplacementData[] ReplacedTypeNameArray { get { return new TypeNameReplacementData[0]; } set => throw new NotImplementedException(); }
     }
 }
