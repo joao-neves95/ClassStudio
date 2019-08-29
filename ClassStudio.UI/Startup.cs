9a455438155210d996cc4c4c855a4d37f1eae511
João Neves <joao95neves@gmail.com>
2019-08-29 21:25:30 +0100

    [UI/Startup.cs]: Added regions + organisation.

--- ClassStudio.UI/Startup.cs
+++ ClassStudio.UI/Startup.cs
@@ -24,19 +24,26 @@ namespace ClassStudio.UI
             Configuration = configuration;
         }
 
+        #region CONFIGURATION
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-                    //.AddJsonOptions( options =>
-                    //{
-                    //    options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
-                    //} );
+            //.AddJsonOptions( options =>
+            //{
+            //    options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+            //} );
 
             services.AddRazorPages();
         }
+
+        #endregion CONFIGURATION
+
+
+        #region CONFIGURE
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -68,15 +75,22 @@ namespace ClassStudio.UI
                 endpoints.MapRazorPages();
             } );
 
-            Task.Run( async () => await Electron.WindowManager.CreateWindowAsync(
-                new BrowserWindowOptions()
-                {
-                    Height = 700,
-                    Width = 900,
-                    Center = true
-                }
-            ) );
+            Task.Run( async () =>
+            {
+                await Electron.WindowManager.CreateWindowAsync(
+                    new BrowserWindowOptions()
+                    {
+                        Height = 700,
+                        Width = 900,
+                        Center = true
+                    } );
+
+                // await UpdateManager._.CheckForUpdates();
+            } );
+
         }
+
+        #endregion CONFIGURE
 
     }
 }
