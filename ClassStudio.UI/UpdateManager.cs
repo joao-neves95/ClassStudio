f1578cc75e8e2c7321c9898969320863c04ad381
João Neves <joao95neves@gmail.com>
2019-08-29 21:26:10 +0100

    Added a singleton class to prepare the update manager.

---
+++ ClassStudio.UI/UpdateManager.cs
@@ -0,0 +1,38 @@
+﻿using ElectronNET.API;
+using ElectronNET.API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassStudio.UI
+{
+    public sealed class UpdateManager
+    {
+
+        #region SINGLETON CONTRUCTOR
+
+        static UpdateManager() { }
+
+        private UpdateManager() { }
+
+        /// <summary>
+        /// 
+        /// The current instance.
+        /// 
+        /// </summary>
+        public static UpdateManager _ { get; } = new UpdateManager();
+
+        #endregion SINGLETON CONTRUCTOR
+
+
+        #region PUBLIC METHODS
+
+        public async Task<UpdateCheckResult> CheckForUpdates()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion PUBLIC METHODS
+    }
+}
