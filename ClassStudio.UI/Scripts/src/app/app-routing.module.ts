04b148d48a032dde8a4928e467ea92e9c59d6066
João Neves <joao95neves@gmail.com>
2019-09-19 20:23:30 +0100

    [UI] - Migrating to Angular 8

--- ClassStudio.UI/Scripts/src/app/app-routing.module.ts
+++ ClassStudio.UI/Scripts/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { GeneratorComponent } from 'src/app/pages/generator/generator.component';
+import { AboutComponent } from 'src/app/pages/about/about.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/generator', pathMatch: 'full' },
+  { path: 'generator', component: GeneratorComponent },
+  { path: 'about', component: AboutComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ClassStudio.UI/Scripts/src/app/app.module.ts
+++ ClassStudio.UI/Scripts/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { GeneratorComponent } from './pages/generator/generator.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    GeneratorComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
