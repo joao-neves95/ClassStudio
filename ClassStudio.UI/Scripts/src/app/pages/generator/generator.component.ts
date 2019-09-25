e9eec2601e840897fdb8752126dc0605df4ec464
João Neves <joao95neves@gmail.com>
2019-09-25 20:48:06 +0100

    [ UI - Angular ]: Added the Generator component
    Everything was migrated to Angular 8, in exception for the Services, which will be next.

--- ClassStudio.UI/Scripts/src/app/pages/generator/generator.component.ts
+++ ClassStudio.UI/Scripts/src/app/pages/generator/generator.component.ts
@@ -1,4 +1,16 @@
+/*
+ * Copyright (c) 2019 João Pedro Martins Neves (shivayl) - All Rights Reserved.
+ *
+ * ClassStudio is licensed under the GNU Lesser General Public License (LGPL),
+ * version 3, located in the root of this project, under the name "LICENSE.md".
+ *
+ */
+
 import { Component, OnInit } from '@angular/core';
+
+import { SelectViewModel, OptionViewModel } from '../../models/select.viewModel';
+import { Utils } from 'src/app/shared/Utils';
+import { LangType } from '../../enums/LangType';
 
 @Component({
   selector: 'app-generator',
@@ -7,9 +19,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GeneratorComponent implements OnInit {
 
-  constructor() { }
+  __inputSelectViewModel: SelectViewModel;
+  __outputSelectViewModel: SelectViewModel;
+
+  inputType: string;
+  outputType: string;
+  outputCode: string = '';
+
+  constructor() {
+
+    this.__inputSelectViewModel = new SelectViewModel(
+      [
+        new OptionViewModel( 'XML', LangType.XML.toString() ),
+        new OptionViewModel( 'C#', LangType.CSharp.toString() ),
+      ]
+    );
+
+    this.inputType = this.__inputSelectViewModel.options[0].value;
+
+    this.__outputSelectViewModel = new SelectViewModel(
+      [
+        new OptionViewModel( 'C#', LangType.XML.toString() ),
+        new OptionViewModel( 'TypeScript', LangType.CSharp.toString() ),
+      ]
+    );
+
+    this.outputType = this.__outputSelectViewModel.options[0].value;
+
+  }
 
   ngOnInit() {
+  }
+
+  /**
+   *
+   * @param selectType ( "input" | "output" )
+   * @param inputType
+   */
+  onSelect(selectType: string, value: string) {
+    selectType = selectType.toUpperCase();
+
+    if (selectType === 'INPUT') {
+      this.inputType = value;
+
+    } else if (selectType === "OUTPUT") {
+      this.outputType = value;
+
+    } else {
+      const exceptionMessage: string = "Unknown generator select type.";
+      this.outputCode = exceptionMessage;
+      throw new Error( exceptionMessage );
+    }
+
+    this.outputCode = `[ GeneratorComponent.onSelect() -> value = ${value} ]`;
+  }
+
+  compile() {
+    const inputCode: string = (<HTMLInputElement>document.getElementById('input-code')).value;
+
+    if (Utils.isNullOrEmpty(inputCode)) {
+      this.outputCode = '[ ERROR: NO INPUT GIVEN ]';
+
+    } else {
+      this.outputCode = inputCode;
+    }
   }
 
 }
---
+++ ClassStudio.UI/Scripts/src/app/pages/generator/generator.service.ts
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2019 João Pedro Martins Neves (shivayl) - All Rights Reserved.
+ *
+ * ClassStudio is licensed under the GNU Lesser General Public License (LGPL),
+ * version 3, located in the root of this project, under the name "LICENSE.md".
+ *
+ */
+
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GeneratorService {
+
+  constructor() { }
+}
