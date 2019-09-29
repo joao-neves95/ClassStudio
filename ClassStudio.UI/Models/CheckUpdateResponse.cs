ce22038821dc890af2e0290b58c4199153616b17
João Neves <joao95neves@gmail.com>
2019-09-29 19:42:17 +0100

    [ UI ]: Created an update system

---
+++ ClassStudio.UI/Models/CheckUpdateResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClassStudio.UI.Models
+{
+    public class CheckUpdateResponse
+    {
+        public bool UpdateAvailable { get; set; }
+
+        public string CurrentVersion { get; set; }
+
+        public string NewAvailableVersion { get; set; }
+
+        public bool Success { get; set; }
+
+        public string ErrorMessage { get; set; }
+    }
+}
---
+++ ClassStudio.UI/Services/UpdateService.cs
@@ -0,0 +1,102 @@
+﻿/*
+ * Copyright (c) 2019 João Pedro Martins Neves (shivayl) - All Rights Reserved.
+ *
+ * ClassStudio is licensed under the GNU Lesser General Public License (LGPL),
+ * version 3, located in the root of this project, under the name "LICENSE.md".
+ *
+ */
+
+using ClassStudio.Core.Services;
+using ClassStudio.UI.Models;
+using ElectronNET.API.Entities;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ClassStudio.UI
+{
+    public sealed class UpdateService
+    {
+
+        #region SINGLETON CONTRUCTOR
+
+        static UpdateService() { }
+
+        private UpdateService() { }
+
+        /// <summary>
+        /// 
+        /// The current instance.
+        /// 
+        /// </summary>
+        public static UpdateService _ { get; } = new UpdateService();
+
+        #endregion SINGLETON CONTRUCTOR
+
+
+        #region PUBLIC METHODS
+
+        /// <summary>
+        /// 
+        /// Returns [true] if there is a new update available, [false] if not, or [null] in case of error.
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public async Task<CheckUpdateResponse> CheckForUpdates()
+        {
+            CheckUpdateResponse checkUpdateResponse = new CheckUpdateResponse();
+
+            try
+            {
+                checkUpdateResponse.CurrentVersion = Startup.CurrentAppVersion;
+
+                string versionPage = await Http.GetAsync( Startup.ElectronManifestJObj.settings.githubVersionFileUrl.Value );
+
+                if (versionPage.StartsWith( "ERROR:" ))
+                {
+                    checkUpdateResponse.Success = false;
+                    checkUpdateResponse.ErrorMessage = versionPage;
+                    return checkUpdateResponse;
+                }
+                else
+                {
+                    checkUpdateResponse.Success = true;
+                    string line;
+                    sbyte lineNum = 0;
+                    using StringReader stringReader = new StringReader( versionPage );
+
+                    while ((line = await stringReader.ReadLineAsync()) != null)
+                    {
+                        lineNum++;
+
+                        if (lineNum == 3)
+                        {
+                            line = line.Replace( "*", String.Empty ).Replace( "v", String.Empty ).Trim();
+                            break;
+                        }
+                    }
+
+                    if (checkUpdateResponse.CurrentVersion != line)
+                    {
+                        checkUpdateResponse.UpdateAvailable = true;
+                        checkUpdateResponse.NewAvailableVersion = line;
+                    }
+                    else
+                    {
+                        checkUpdateResponse.UpdateAvailable = false;
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                checkUpdateResponse.Success = false;
+                checkUpdateResponse.ErrorMessage = e.Message + "\n" + e.StackTrace;
+            }
+
+            return checkUpdateResponse;
+        }
+
+        #endregion PUBLIC METHODS
+    }
+}
