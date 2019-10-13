c7bfaf1de014e771c2bf5f32aa317cc8a04ea310
João Neves <joao95neves@gmail.com>
2019-10-13 17:29:05 +0100

    [ UI - ng ]: Refactored the Generator service to compile files and dirs

---
+++ ClassStudio.UI/Scripts/src/app/services/generator.service.ts
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2019 João Pedro Martins Neves (shivayl) - All Rights Reserved.
+ *
+ * ClassStudio is licensed under the GNU Lesser General Public License (LGPL),
+ * version 3, located in the root of this project, under the name "LICENSE.md".
+ *
+ */
+
+import { Injectable, Output } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+
+import { GeneratorDTO } from '../models/DTO/generatorDTO';
+
+import { Constants } from '../constants';
+import { Utils } from '../shared/utils';
+import { LangType } from '../enums/LangType';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GeneratorService {
+
+  // #region PROPERTIES
+
+  private readonly baseGeneratorUrl: string = 'api/generator/';
+
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json;charset=UTF-8',
+      'Accept': 'application/json, text/plain'
+    } )
+  };
+
+  // #endregion PROPERTIES
+
+  constructor( private http: HttpClient ) { }
+
+  // #region PUBLIC METHODS
+
+  /**
+   * POST request to compile code in the GeneratorController.
+   */
+  compile( inputCode: string | null | undefined, inputType: number | null | undefined, outputType: number | null | undefined ): Observable<string> {
+
+    if ( Utils.isNullOrEmpty( inputCode ) ) {
+      return of( Constants.ErrorMessages.WrongInput );
+    }
+
+    return this.postInputToServer( new GeneratorDTO( inputCode as string ), inputType, outputType );
+  }
+
+  compileFiles( inputFiles: string[], inputAreFiles: boolean, inputType: number | null | undefined, outputType: number | null | undefined ) {
+
+    if ( Utils.isNullOrEmptyArr( inputFiles ) ) {
+      return of( Constants.ErrorMessages.WrongInput );
+    }
+
+    return this.postInputToServer( new GeneratorDTO( null, inputAreFiles, inputFiles ), inputType, outputType );
+  }
+
+  // #endregion PUBLIC METHODS
+
+  // #region PRIVATE METHODS
+
+  private postInputToServer( dto: GeneratorDTO, inputType: number | null | undefined, outputType: number | null | undefined  ): Observable<string> {
+
+    if ( Utils.isNullNmbr( inputType ) || Utils.isNullNmbr( outputType ) ) {
+      return of( Constants.ErrorMessages.WrongInput );
+    }
+
+    const endpoint: string = this.getEndpoint( inputType as number, outputType as number );
+
+    if (endpoint === '')  {
+      return of( Constants.ErrorMessages.NotImplemented );
+    }
+
+    return this.http.post<string>( this.baseGeneratorUrl + endpoint, dto, this.httpOptions );
+  }
+
+  // #endregion PRIVATE METHODS
+
+  // #region PRIVATE HELPER METHODS
+
+  private getEndpoint( inputType: number, outputType: number ): string {
+
+    if ( inputType == LangType.XML && outputType == LangType.CSharp ) {
+      return 'XMLStringToCSharp';
+
+    } else if ( inputType == LangType.CSharp && outputType == LangType.TypeScript ) {
+      return 'CSharpToTypescript';
+
+    } else {
+      return '';
+    }
+
+  }
+
+  // #endregion PRIVATE HELPER METHODS
+}
