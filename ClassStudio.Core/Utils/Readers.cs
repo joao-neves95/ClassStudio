d6a65fda6ee81ef7c8074f05d30f1abad10bf8ef
João Neves <joao95neves@gmail.com>
2019-10-13 21:14:25 +0100

    [ Core ]: Added the Readers class with multiple IO reading utils

---
+++ ClassStudio.Core/Utils/Readers.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassStudio.Core.Utils
+{
+    public static class Readers
+    {
+        public static string[] ReadDirectory(string[] directoryPaths, string extension = "*.*")
+        {
+            List<string> allFilePaths = new List<string>();
+
+            for (int i = 0; i < directoryPaths.Length; ++i)
+            {
+                allFilePaths.AddRange( Readers.ReadDirectory( directoryPaths[i], extension ) );
+            }
+
+            return allFilePaths.ToArray();
+        }
+
+        /// <summary>
+        /// 
+        /// Returns an array of all the files in the provided directory that matches the extension pattern.
+        /// 
+        /// </summary>
+        /// <param name="directoryPath"></param>
+        /// <param name="extension"> Eg: "*.*" = all; "*.XML" = all .xml files </param>
+        /// <returns></returns>
+        public static string[] ReadDirectory(string directoryPath, string extension = "*.*")
+        {
+            return Directory.GetFiles( directoryPath, extension );
+        }
+
+        /// <summary>
+        /// 
+        /// Reads all provided files and returns them in a single string.
+        /// 
+        /// </summary>
+        /// <param name="filePaths"></param>
+        /// <returns></returns>
+        public static async Task<string[]> ReadFileAsync(string[] filePaths)
+        {
+            for (int i = 0; i < filePaths.Length; ++i)
+            {
+                filePaths[i] = await Readers.ReadFileAsync( filePaths[i] );
+            }
+
+            return filePaths;
+        }
+
+        public static async Task<string> ReadFileAsync(string filePath)
+        {
+            return await File.ReadAllTextAsync( filePath, Encoding.UTF8 );
+        }
+    }
+}
