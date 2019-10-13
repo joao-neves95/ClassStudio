ffb0c347b71c054fc6961f2beaf577f39d2ce3e1
João Neves <joao95neves@gmail.com>
2019-10-13 17:34:30 +0100

    [ UI - ng ]: Added a constants file

---
+++ ClassStudio.UI/Scripts/src/app/constants.ts
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2019 João Pedro Martins Neves (shivayl) - All Rights Reserved.
+ *
+ * ClassStudio is licensed under the GNU Lesser General Public License (LGPL),
+ * version 3, located in the root of this project, under the name "LICENSE.md".
+ *
+ */
+
+
+export const Constants = Object.freeze({
+    ErrorMessages: {
+        WrongInput: '[ INVALID INPUT ]',
+        NotImplemented: '[ NOT IMPLEMENTED ]'
+    }
+});
